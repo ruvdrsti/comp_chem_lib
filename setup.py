08aab01b69b8f0dc71d19a0907ee17393fb79272
ruvdrsti <ruben.vanderstichelen@ugent.be>
2021-02-24 15:18:09 +0100

    some extra setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name=’comp_chem’,
+    packages=find_packages(include=['mypythonlib']),
+    version=’0.1.0',
+    description=’bacherlor project @ ugent GQCG’,
+    author=’ruvdrsti’,
+    license=’MIT’,
+)
---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+def readData(filename):
+    """
+    Reads files
+    
+    input:
+    filename: the path to the file you want opened
+
+    output:
+    tuple of arrays containing atomic numbers and atomic coords
+    """
+    rawdata = np.loadtxt(filename, skiprows=1)
+    atomic_numbers = rawdata[:, 0]
+    atomic_coords = rawdata[:, 1:]
+    return atomic_numbers, atomic_coords
