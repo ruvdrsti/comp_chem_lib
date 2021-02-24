9716675d871159a7d3f8812b4d52769174048d7c
ruvdrsti <ruben.vanderstichelen@ugent.be>
2021-02-24 15:20:47 +0100

    correcting errors

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import find_packages, setup
 setup(
-    name=’comp_chem’,
+    name='comp_chem',
     packages=find_packages(include=['mypythonlib']),
-    version=’0.1.0',
-    description=’bacherlor project @ ugent GQCG’,
-    author=’ruvdrsti’,
-    license=’MIT’,
+    version='0.1.0',
+    description='bacherlor project @ ugent GQCG',
+    author='ruvdrsti',
 )
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import numpy as np
 def readData(filename):
     """
     Reads files
