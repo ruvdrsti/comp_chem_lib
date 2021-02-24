d2cd48ac9ce1ab4f7789139e96f9b1d795a9dc9f
ruvdrsti <ruben.vanderstichelen@ugent.be>
2021-02-24 15:54:40 +0100

    refactoring

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='comp_chem',
+    name='compChem',
     packages=find_packages(include=['mypythonlib']),
     version='0.1.0',
     description='bacherlor project @ ugent GQCG',
