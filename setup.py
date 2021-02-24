d272335b80d2627a180d8fa4313f22de4932cb91
ruvdrsti <ruben.vanderstichelen@ugent.be>
2021-02-24 16:11:22 +0100

    adding the right folder this time

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='compChem',
-    packages=find_packages(include=['mypythonlib']),
+    packages=find_packages(include=['compChem']),
     version='0.1.0',
     description='bacherlor project @ ugent GQCG',
     author='ruvdrsti',
