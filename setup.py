bf6a8f5cc3c270a24ef5c5fadf2c46c944769c23
ruvdrsti <ruben.vanderstichelen@ugent.be>
2021-02-27 14:36:40 +0100

    next round of testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='compChem',
     packages=find_packages(include=['compChem']),
-    version='0.2.0',
+    version='0.2.1',
     description='bacherlor project @ ugent GQCG',
     author='ruvdrsti',
 )
