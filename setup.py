c7dbc8a7473d5097db1749d6b6ea9fd576d6f35b
ruvdrsti <ruben.vanderstichelen@ugent.be>
2021-03-04 15:48:49 +0100

    updating version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='compChem',
     packages=find_packages(include=['compChem']),
-    version='0.2.2',
+    version='0.3.0',
     description='bacherlor project @ ugent GQCG',
     author='ruvdrsti',
 )
