186e93867ce3fa205795119429b8272ad8a61e91
ruvdrsti <ruben.vanderstichelen@ugent.be>
2021-04-27 16:58:04 +0200

    adding spin contamination to rhf mode

--- build/lib/compChem/Hartree_Fock_modes/rhf.py
+++ build/lib/compChem/Hartree_Fock_modes/rhf.py
@@ -111,3 +111,34 @@ class RHFMolecule(Molecule):
         return E_total, itercount
 
 
+    def getSpinContamination(self, mixedGuess=True):
+        """Will display the spin contamination
+
+        input:
+        mixedGuess: False if you do not want to use a mixed guess
+        """
+        # transform p to MO basis, where mo basis = the eigenfunctions of the f_a operator
+        a = self.getDensityMatrix()
+        b = self.getDensityMatrix()
+        f_a, f_b = self.displayFockMatrix(), self.displayFockMatrix()
+        p = (a+b)/2
+        m = (a-b)/2
+        c = sp.eigh(f_a, self.overlap)[1] # we only need the c matrix, not the eigenvalues themselves,
+
+
+        # pay attention, c matrices are not unitary
+        c_inv = np.linalg.inv(c) # we need the inverse for later
+        p_trans = np.einsum("pq, qr, rs->ps", c_inv, p, c_inv.T, optimize=True)
+        m_trans = np.einsum("pq, qr, rs->ps", c_inv, m, c_inv.T, optimize=True)
+
+
+        # transform the fock matrices to NO basis
+        d = sp.eigh(p_trans)[1]
+        d = d[:, ::-1] #invert all collumns
+
+        d_inv = np.linalg.inv(d)
+        m_no = np.einsum("pq, qr, rs->ps", d_inv, m_trans, d_inv.T, optimize=True)
+
+        return self.beta - (self.alpha + self.beta)/2 + 2*np.trace(m_no.dot(m_no))
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='compChem',
     packages=find_packages(include=['compChem']),
-    version='0.7.3',
+    version='0.7.4',
     description='bachelor project @ ugent GQCG',
     author='ruvdrsti',
 )
